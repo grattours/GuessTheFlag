fe63d28f120bd6a6a37c3a41e0a58c3eb127309c
Grattours <grattours@gmail.com>
2019-10-29 00:21:25 +0100

    J34 Animations

--- GuessTheFlag/ContentView.swift
+++ GuessTheFlag/ContentView.swift
@@ -4,7 +4,7 @@
 //
 //  Created by Luc Derosne on 18/10/2019.
 //  Copyright © 2019 Luc Derosne. All rights reserved.
-//
+//SwiftUI GuesTheflag avec animations J34
 
 import SwiftUI
 
@@ -15,13 +15,15 @@ struct ContentView: View {
     @State private var scoreTitle = ""
     @State private var countries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Nigeria", "Poland", "Russia", "Spain", "UK", "US"].shuffled()
     @State private var correctAnswer = Int.random(in: 0...2)
+    @State private var rotation = 0.0
+    @State private var isCorrect = false
+    @State private var opacity = 1.0
+    @State private var currentAnswer = 0
     
     var body: some View {
         ZStack {
-            //Color.blue.edgesIgnoringSafeArea(.all)
-            LinearGradient(gradient: Gradient(colors: [.blue, .black]), startPoint: .top, endPoint: .bottom)
-                .edgesIgnoringSafeArea(.all)
-            
+            Color.blue.edgesIgnoringSafeArea(.all)
+            AngularGradient(gradient: Gradient(colors: [.blue, .black]), center: .bottomTrailing).edgesIgnoringSafeArea(.all)
             VStack(spacing: 30) {
                 VStack {
                     Text("clic le drapeaux de : ")
@@ -33,38 +35,52 @@ struct ContentView: View {
                 }
                 ForEach(0 ..< 3) { number in
                     Button(action: {
+                        self.opacity = 0.25
                         self.flagTapped(number)
+                        self.rotation = 0
+                        if self.isCorrect {
+                            withAnimation {
+                                self.rotation = 360
+                            }
+                        } else {
+                            self.rotation = 0
+                            
+                        }
                     }) {
                         FlagImage(country: self.countries[number])
-//                        Image(self.countries[number])
-//                            .renderingMode(.original)
-//                            .clipShape(Capsule())
-//                            .overlay(Capsule().stroke(Color.black, lineWidth: 1))
-//                            .shadow(color: .black, radius: 2)
                     }
+                    .opacity((number == self.correctAnswer) ? 1 : self.opacity)
+                    .animation(.default)
+                    .rotation3DEffect(.degrees((number == self.correctAnswer) ? self.rotation : 0), axis: (x: 0, y: 1, z: 0))
+                    .animation(.easeInOut(duration: 1.0), value: self.isCorrect)
                 }
                 Text("Score: \(userScore)")
                     .foregroundColor(.white)
                 Spacer()
             }
         }.alert(isPresented: $showingScore) { () -> Alert in
-            Alert(title: Text(scoreTitle), message: Text(scoreMessage), dismissButton: .default(Text("Continue")) { self.askQuestion() })
+            Alert(title: Text(scoreTitle), message: Text(scoreMessage), dismissButton: .default(Text("Continuer")) { self.askQuestion() })
         }
     }
     func flagTapped(_ number: Int) {
+        currentAnswer = number
         if number == correctAnswer {
+            isCorrect = true
             scoreTitle = "Correct"
             userScore += 1
             scoreMessage = "Bravo, ton score passe à \(userScore)"
         } else {
+            isCorrect = false
             scoreTitle = "Faux"
-             scoreMessage = "Dommage, c'est le drapeau de \(countries[number])"
+            scoreMessage = "Dommage, c'est le drapeau de \(countries[number])"
         }
         
         showingScore = true
     }
     
     func askQuestion() {
+        rotation = 0
+        opacity = 1
         countries.shuffle()
         correctAnswer = Int.random(in: 0...2)
     }
@@ -76,8 +92,10 @@ struct FlagImage: View {
     var body: some View {
         Image(country)
             .renderingMode(.original)
-            .clipShape(Capsule())
-            .overlay(Capsule().stroke(Color.black, lineWidth: 1))
+            //.clipShape(Capsule())
+            .clipShape(RoundedRectangle(cornerRadius: 8.0))
+            .overlay(RoundedRectangle(cornerRadius: 8.0).stroke(Color.black, lineWidth: 1))
+            //.overlay(Capsule().stroke(Color.black, lineWidth: 1))
             .shadow(color: .black, radius: 2)
     }
 }
